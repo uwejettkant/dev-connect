dca1f2b2dbba85fdd84d99687def72cfbceb7868
Uwe Jettkant <ujettkant@gmail.com>
2025-11-05 18:10:40 +0100

    Implement initial page layout and add Explore button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <h1>Welcome to Next.js!</h1>;
-}
+import ExploreBtn from '@/components/ExploreBtn';
+
+const Page = async () => {
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event You Can&apos;t Miss
+      </h1>
+      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+
+      <ExploreBtn />
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {[1, 2, 3, 4, 5].map((event) => (
+            <li key={event} className="event-card">
+              <h4>Event {event}</h4>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Page;
