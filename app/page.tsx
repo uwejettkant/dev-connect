c86710b44d6d058af23d77a84b2ae1e1155b70de
Uwe Jettkant <ujettkant@gmail.com>
2025-11-05 18:47:02 +0100

    Added the EventCard component and implement the display of events on the page. Added contestants file in lib folder which contains the data

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import ExploreBtn from '@/components/ExploreBtn';
+import EventCard from '@/components/EventCard';
+import events from '@/lib/constants';
 
 const Page = async () => {
   return (
@@ -9,13 +11,14 @@ const Page = async () => {
       <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
 
       <ExploreBtn />
+
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
 
         <ul className="events">
-          {[1, 2, 3, 4, 5].map((event) => (
-            <li key={event} className="event-card">
-              <h4>Event {event}</h4>
+          {events.map((event) => (
+            <li key={event.title} className="list-none">
+              <EventCard {...event} />
             </li>
           ))}
         </ul>
